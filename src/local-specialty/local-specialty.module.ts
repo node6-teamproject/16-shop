f869e85de57a7f4fba7f4b98601b87f61836cd94
wkddudghks81 <wkddudghks81@naver.com>
2024-11-27 16:04:33 +0900

    resolve JwtService dependency issue in LocalSpecialtyModule

--- src/local-specialty/local-specialty.module.ts
+++ src/local-specialty/local-specialty.module.ts
@@ -5,12 +5,23 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { LocalSpecialty } from './entities/local-specialty.entity';
 import { UserModule } from 'src/user/user.module';
 import { AuthModule } from 'src/auth/auth.module';
-import { RolesGuard } from '../auth/guards/roles.guard';
+import { UserService } from '../user/user.service';
+import { User } from 'src/user/entities/user.entity';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([LocalSpecialty]), AuthModule, forwardRef(() => UserModule)],
+  imports: [TypeOrmModule.forFeature([LocalSpecialty]),TypeOrmModule.forFeature([User]), forwardRef(() =>AuthModule), forwardRef(() => UserModule)
+  ,JwtModule.registerAsync({
+    useFactory: (config: ConfigService) => ({
+      secret: config.get<string>('JWT_SECRET'),
+    }),
+    inject: [ConfigService],
+  }),
+],
   controllers: [LocalSpecialtyController],
-  providers: [RolesGuard,LocalSpecialtyService],
-  exports: [LocalSpecialtyService],
+  providers: [RolesGuard,LocalSpecialtyService,UserService],
+  exports: [RolesGuard,TypeOrmModule,LocalSpecialtyService],
 })
 export class LocalSpecialtyModule {}
