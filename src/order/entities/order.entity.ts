5398c0a5a96093a26da0e55d5ae191fea731f54e
NSHkw <64311828+NSHkw@users.noreply.github.com>
2024-12-04 14:28:58 +0900

    add: order, cart function

--- src/order/entities/order.entity.ts
+++ src/order/entities/order.entity.ts
@@ -16,7 +16,21 @@ export enum ShipStatus {
   SHIPPING = '배송 중',
   SHIP_WAITING = '배송 대기',
   ORDER_COMPLETED = '주문 완료',
+  ORDER_CANCELLED = '주문 취소',
+  PAYMENT_WAITING = '결제 대기',
 }
+
+export enum OrderMethod {
+  DELIVERY = '일반 택배',
+  PICKUP = '직접 수령',
+  POST_OFFICE = '우체국 택배',
+}
+
+export enum OrderType {
+  DIRECT = 'DIRECT', // 직접 구매
+  CART = 'CART', // 장바구니 구매
+}
+
 @Entity()
 export class Order {
   @PrimaryGeneratedColumn({ type: 'int', unsigned: true })
@@ -25,26 +39,34 @@ export class Order {
   @Column({ type: 'int', unsigned: true })
   user_id: number;
 
-  @Column({ type: 'enum', enum: ShipStatus })
+  @Column({ type: 'enum', enum: ShipStatus, default: ShipStatus.PAYMENT_WAITING })
   status: ShipStatus;
 
-  @Column()
+  @Column({ type: 'varchar' })
   order_address: string;
 
-  @Column()
-  order_method: string;
+  @Column({ type: 'enum', enum: OrderMethod })
+  order_method: OrderMethod;
 
-  @Column()
+  @Column({ type: 'bigint', default: 0 })
   total_cash: number;
 
-  @Column()
+  @Column({
+    type: 'enum',
+    enum: OrderType,
+    default: OrderType.DIRECT,
+    comment: '주문 타입 (직접 구매/장바구니 구매)',
+  })
+  order_type: OrderType;
+
+  @Column({ type: 'datetime' })
   order_date: Date;
 
   @CreateDateColumn()
   created_at: Date;
 
-  @UpdateDateColumn()
-  updated_at: Date;
+  @UpdateDateColumn({ nullable: true })
+  updated_at?: Date;
 
   @ManyToOne(() => User, (user) => user.orders)
   @JoinColumn({ name: 'user_id' })
